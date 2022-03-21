74a02c196702c91acaf9d3aa373284dc8a304eaa
Rick Yakubowski <ryakubowski@gmail.com>
2022-03-21 13:06:15 -0400

    Proper Composition API implementation; Updated README; Version bump;

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
 # vue-gpu-fingerprint
 
-Get a browser's fingerprint (vendor and hardware) data from a Vue component using WebGL and the GPU.
+Get a browser's fingerprint (vendor and hardware) data from a Vue component using WebGL
+and the GPU. Some basic browser data is included as well.
+
+## Stats
+
+![Current NPM Version](https://flat.badgen.net/npm/v/vue-gpu-fingerprint)
+![Current NPM Downloads](https://flat.badgen.net/npm/dt/vue-gpu-fingerprint)
+![License](https://flat.badgen.net/npm/license/vue-gpu-fingerprint)
+![Types](https://flat.badgen.net/npm/types/vue-gpu-fingerprint)
+
+## Purpose
+
+I needed a decent way (component-wise) to gather some device data to include in a JWT. The device data is very basic,
+doesn't personally identify the user (obviously), and can likely be used for other purposes as well. It's just good to know!
+
+# Install
+
+## NPM
+
+In your project's root directory...
+
+```bash
+npm install --save vue-gpu-fingerprint
+```
+
+## Git
+
+### Cloning the project
+
+```bash
+git clone git@github.com:yakubori/vue-gpu-fingerprint.git
+```
+
+### Building
+
+From the project root, install the depndencies, then run the build
+
+```bash
+npm install
+```
+
+```bash
+npm run build
+```
+
+### Running the sample server
+
+From the project root, install the depndencies, then run the sample server.
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
 
 ## Example Usage
 
@@ -8,7 +63,7 @@ Very simple. Just handle the `GpuFingerprint` event on the FingerPrint component
 
 The event-emit occurs when the FingerPrint component first mounts.
 
-### Using TS/JS
+### Using TS
 
 ```typescript
 import { createApp, h } from "vue"
@@ -25,8 +80,29 @@ const app = createApp({
 app.mount('#root')
 ```
 
+### Using JS
+
+```javascript
+import { createApp, h } from "vue"
+import FingerPrint from 'vue-gpu-fingerprint'
+
+const app = createApp({
+    render() {
+        return h(FingerPrint, {
+            onGpuFingerprint: (data) => { console.log(data) }
+        })
+    }
+})
+
+app.mount('#root')
+```
+
 ### Using in a SFC
 
-```vue
-<FingerPrint @GpuFingerprint="handleGpuFingerprint" />
+```jsx
+<FingerPrint @gpu-fingerprint="handleGpuFingerprint" />
 ```
+
+## Contributing
+
+I realize this is a simple module, but I'm totally open to others working on this. Feel free to create your own branch and submit a pull request.
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,37 @@
-import { h, defineComponent } from "vue"
+import { defineComponent, h } from "vue"
 
 export default defineComponent({
     emits: ['GpuFingerprint'],
-    methods: {
-        getFingerprint() {
+    setup(_props: {}, ctx: any) {
+        const getFingerprint = (canvas: any) => {
             let renderer, vendor;
-            const context = this.$el.getContext('webgl');
-            const unpackData = context.getExtension("WEBGL_debug_renderer_info");
-            if (unpackData != null) {
-                renderer = context.getParameter(unpackData.UNMASKED_RENDERER_WEBGL);
-                vendor = context.getParameter(unpackData.UNMASKED_VENDOR_WEBGL);
-            }
-            return {
-                vendor,
-                renderer,
-                browser: {
-                    vendor: navigator.vendor,
-                    userAgent: navigator.userAgent,
-                    language: navigator.language,
-                    hardwareConcurrency: navigator.hardwareConcurrency,
+            const context = canvas.el.getContext('webgl');
+            if (context !== null) {
+                const unpackData = context.getExtension("WEBGL_debug_renderer_info");
+                if (unpackData != null) {
+                    renderer = context.getParameter(unpackData.UNMASKED_RENDERER_WEBGL);
+                    vendor = context.getParameter(unpackData.UNMASKED_VENDOR_WEBGL);
+                }
+                return {
+                    vendor,
+                    renderer,
+                    browser: {
+                        vendor: navigator.vendor,
+                        userAgent: navigator.userAgent,
+                        language: navigator.language,
+                        hardwareConcurrency: navigator.hardwareConcurrency,
+                    }
                 }
             }
-        },
-        raiseFingerprint() {
-            const fp = this.getFingerprint();
-            this.$emit('GpuFingerprint', fp);
-        }
-    },
-    mounted() {
-        this.raiseFingerprint();
-    },
-    render() {
-        return h('canvas', {
+        };
+
+        return () => h('canvas', {
             width: 1,
             height: 1,
-            style: "display: none; width: 1px; height: 1px;"
+            style: "display: none; width: 1px; height: 1px;",
+            onVnodeMounted: (el) => {
+                ctx.emit('GpuFingerprint', getFingerprint(el))
+            },
         })
     },
 })
